433ab506a8cbc811d8148765069625d60f1ac776
Anthony Mark <anthonymark@31-33-111.wireless.csail.mit.edu>
2013-11-03 17:21:18 -0500

    last update day 1

--- day1.js
+++ day1.js
@@ -1,5 +1,22 @@
-function clickHandler(index) {
-                var whatsInside = $("#text"+index).val();  
+function clickHandler() {
+                var whatsInside = $("#text").val();  
                 $("#text_out").html(whatsInside.toUpperCase());
                 console.log(whatsInside.toUpperCase());
             }
+
+$(document).ready(function() {
+    $(".calculator").each(function(){
+        var inputBox = $('<input type="text" size="50">');
+        var calcButton = $('<p><button>Calculate</button></p>')
+        var output = $('<div id="text_out1"></div>')
+        
+        calcButton.on("click", function() {
+            var whatsInside = inputBox.val()
+            output.text(whatsInside.toUpperCase())
+        });
+        
+        $(this).append(inputBox);
+        $(this).append(calcButton);
+        $(this).append(output);
+    })
+});
